0d3862255318232ea534bb360d7e4b8470d2dd6a
WayneHoward <wayne_howard@outlook.com>
2021-02-22 14:09:47 +0800

    2-22 14:09

--- src/components/Goods/parameter.vue
+++ src/components/Goods/parameter.vue
@@ -1,20 +1,28 @@
-<!--
- * @Author: your name
- * @Date: 2021-02-21 19:05:01
- * @LastEditTime: 2021-02-21 19:08:15
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \项目\后台管理系统\management\src\components\Goods\parameter.vue
--->
 <template>
-  <div class="parameter">
-      分类参数
-  </div>
+    <div class="parameter">
+        <!-- 商品分类 -->
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
 </script>
 
 <style scoped lang="less">
+
 </style>
